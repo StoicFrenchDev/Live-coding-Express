59fdc0667c74316547d20cce54eebc79711cfa07
StoicFrenchDev <richard.benjamin69@yahoo.fr>
2024-04-09 10:56:32 +0200

    up

--- index.js
+++ index.js
@@ -16,32 +16,47 @@ app.get("/toto", (req, res) => {
   });
 
   
-const things = [
-    { id: 1, name: "Socks" },
-    { id: 2, name: "Skills" },
-    { id: 3, name: "Passion" },
-    { id: 3, name: "Passion" },
-    { id: 3, name: "Passion" },
-    { id: 3, name: "Passion" },
-    { id: 3, name: "Passion" },
-    { id: 3, name: "Passion" },
-    { id: 3, name: "Passion" },
+const cats = [
+    { id: 1, name: "Thierry", color: "blanc" },
+    { id: 2, name: "Jean", color: "noir"  },
+    { id: 3, name: "Léa", color: "noir"  },
+    { id: 4, name: "Matthieu", color: "blanc"  },
+    { id: 5, name: "Marcotoine", color: "blanc"  },
+    { id: 6, name: "Amina", color: "gris"  },
+    { id: 7, name: "Gwen", color: "noir"  },
+    { id: 8, name: "Michel", color: "noir"  },
+    { id: 9, name: "Jeromine", color: "blanc"  },
+    { id: 10, name: "Anne", color: "noir"  },
+    { id: 11, name: "Sophie", color: "blanc"  },
+    { id: 10, name: "Adeline", color: "gris"  },
   ];
   
-  app.get("/things", (req, res) => {
-    res.json(things);
+  app.get("/cats", (req, res) => {
+    console.log(req.query);
+    let result = cats.slice();
+
+    // Ajout filtre
+    if (req.query.color) {
+        result = result.filter(cat => cat.color === req.query.color);
+    }
+
+    // Ajout limite
+    const limit = parseInt(req.query.limit) || 10; // 10 par défaut si non spécifié ou invalide
+    result = result.slice(0, limit);
+
+    res.json(result);
   });
 
-  app.get('/things/:id', (req, res) => {
+  app.get('/cats/:id', (req, res) => {
 
     const wantedId = parseInt(req.params.id)
   
-    const thing = things.find(
-      (thing) => thing.id === wantedId
+    const cat = cats.find(
+      (cat) => cat.id === wantedId
     );
   
-    if (thing != null) {
-      res.json(thing);
+    if (cat != null) {
+      res.json(cat);
     } else {
       res.sendStatus(404);
     }
